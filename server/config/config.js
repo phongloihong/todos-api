1683451713ed6b1faadf805b19dac7159a56e90a
Loi Hong Phong <loihongphong@Lois-MacBook-Pro.local>
2017-04-24 15:46:08 +0700

    finish

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 const env = process.env.NODE_ENV || 'development';
 
 if (env ==='development' || env === 'test') {
-  const config = require('./congig.json');
+  const config = require('./config.json');
   const envConfig = config[env];
 
   Object.keys(envConfig).forEach((key) => {
